04f5d75f58b35516734c84eb93864cba19d3df00
冯垚杰 <yaojie.feng@symbio.com>
2021-10-11 10:02:44 +0800

    修改前缀

--- setup/ConfigureiOS.rb
+++ setup/ConfigureiOS.rb
@@ -53,7 +53,7 @@ module Pod
 #          end
 #      end
      keep_demo = :yes
-      prefix = nil
+      prefix = "YJ"
 
 #      loop do
 #        prefix = configurator.ask("What is your class prefix").upcase
